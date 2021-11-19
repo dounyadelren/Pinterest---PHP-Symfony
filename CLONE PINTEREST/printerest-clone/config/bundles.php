ce5cc30198a17bc115965ca83181c7d8ddb257f8
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2021-11-19 16:00:40 +0100

    [update] add security bundle to create user interface

--- CLONE PINTEREST/printerest-clone/config/bundles.php
+++ CLONE PINTEREST/printerest-clone/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
