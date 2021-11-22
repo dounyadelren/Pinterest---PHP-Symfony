83e6276a2c72240948f0fee2baab61b19ed16fea
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2021-11-22 12:41:36 +0100

    [update] registrationform.php, trying to cope with token bug

--- CLONE PINTEREST/printerest-clone/src/Form/RegistrationFormType.php
+++ CLONE PINTEREST/printerest-clone/src/Form/RegistrationFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -16,6 +17,7 @@ class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        
         $builder
             ->add('email')
             ->add('firstName')
